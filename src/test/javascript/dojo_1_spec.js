54683115af24a85da5a2b83ca5a2cc67dfcaa425
Rémy-Christophe Schermesser <git@schermesser.com>
2013-07-05 12:35:02 +0200

    tests

--- src/test/javascript/dojo_1_spec.js
+++ src/test/javascript/dojo_1_spec.js
@@ -11,11 +11,11 @@ describe('dojo 1', function() {
 		});
 
 		it('3 façons de définir une fonction', function() {
-		  //Ecrire de 3 façons une fonction
+			//Ecrire de 3 façons une fonction
 
-      expect(fonction1 instanceof Function).toBeTruthy();
-      expect(fonction2 instanceof Function).toBeTruthy();
-      expect(fonction3 instanceof Function).toBeTruthy();
+			expect(fonction1 instanceof Function).toBeTruthy();
+			expect(fonction2 instanceof Function).toBeTruthy();
+			expect(fonction3 instanceof Function).toBeTruthy();
 		});
 
 		var a = 1;
@@ -26,7 +26,7 @@ describe('dojo 1', function() {
 
 			function maFonction() {
 				var a = 3;
-				b = 2;
+				b = 3;
 
 				//Ecrivez deux tests sur les valeurs de a et b
 			}
@@ -43,7 +43,19 @@ describe('dojo 1', function() {
 			expect(fonction instanceof Function).toBeTruthy();
 
 			expect(objet instanceof Object).toBeTruthy();
+			expect(objet instanceof Function).toBeFalsy();
+		});
+
+		it('all is object (or not)', function() {
+			//Ecrivez ce qui fait passer le test
+
+			expect(fonction instanceof Object).toBeTruthy();
+			expect(fonction instanceof Function).toBeTruthy();
+
+			expect(objet instanceof Object).toBeTruthy();
 			expect(objet instanceof Object).toBeFalsy();
+
+			expect(fonction.maPropriete).toBe(objet.maPropriete);
 		});
 	});
 
@@ -53,7 +65,7 @@ describe('dojo 1', function() {
 				//ecrivez un test qui compare ninja et myNinja
 			}
 
-			//ecrivez un test sur la valeure de ninja et myNinja
+			//ecrivez un test sur la valeur de ninja et myNinja
 		});
 
 		it("arguments un object caché", function() {
@@ -62,6 +74,86 @@ describe('dojo 1', function() {
 			}
 			console.log(maFonction(1));
 			
+		});
+	});
+
+	describe("Les objets c'est cool aussi", function() {
+		it("l'operateur new", function() {
+			function Ninja(){
+				this.name = "Ninja";
+			}
+
+			//Faites passer les tests
+			var ninjaA = Ninja();
+			expect(ninja).toBe( ?? );
+
+			var ninjaB = new Ninja();
+			expect(ninja.name).toBe( ?? );
+		});
+
+		it("", function() {
+			function Ninja(){ 
+				//N'oubliez pas la propriété swung
+
+ 				this.swingSword = function(){ 
+					//Faites passer les tests
+				}; 
+			} 
+			
+			var ninja = new Ninja(); 
+			expect(ninja.swingSword()).toBeTruthy();
+			expect(ninja.swung).toBeTruthy();
+			 
+			var ninjaB = new Ninja(); 
+			expect(ninjaB.swung).toBeFalsy();
+		});
+	});
+
+	describe("Pour aller plus loin", function() {
+		it("mettre en cache le résultat d'une fonction", function() {
+			function isPrime( num ) { 
+				var prime = (num != 1); // Everything but 1 can be prime 
+				for ( var i = 2; i < num; i++ ) { 
+					if ( num % i == 0 ) { 
+						prime = false; 
+						break; 
+					} 
+				} 
+				return prime; 
+			}
+
+			//Modifiez la méthode au dessus pour faire passer les tests
+			expect(isPrime(5)).toBeTruthy();
+			expect(isPrime.cache[5]).toBeTruthy();
+		});
+
+		it("Parlons contexte", function() {
+			var object = {}; 
+			function fn(){ 
+				return this; 
+			}
+
+			//Faites passer les tests
+			expect(fn()).toBe( ?? );
+
+			//Changez le contexte d'appel de fn par object
+			expect( ?? ).toBe( object );
+		})
+
+		it("", function() {
+			function loop(array, fn){ 
+				for ( var i = 0; i < array.length; i++ ) { 
+					// Implementez la méthode pour faire passer les tests
+				} 
+			} 
+
+			var num = 0; 
+			loop([0, 1, 2], function(value){ 
+				expect(value).toBe(num);
+				expect(this instanceof Array).toBeTruthy();
+
+				num++;
+			});
 		});
 	});
 
