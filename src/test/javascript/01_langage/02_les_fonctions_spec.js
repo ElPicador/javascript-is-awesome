1a9d86e4d56e36d28a947e6ba2bbf75a25ade9f5
Rémy-Christophe Schermesser <git@schermesser.com>
2013-07-08 14:11:56 +0200

    Refactoring

---
+++ src/test/javascript/01_langage/02_les_fonctions_spec.js
@@ -0,0 +1,11 @@
+
+describe("les fonctions", function() {
+	it("Quel est le nom de ma fonction ?", function() {
+		var ninja = function myNinja() {
+			//ecrire un test qui compare ninja et myNinja
+		}
+
+		//ecrire un test sur la valeur de ninja et myNinja
+	});
+
+});
