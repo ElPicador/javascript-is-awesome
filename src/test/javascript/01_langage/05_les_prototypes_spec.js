26916cd762df4f74181bb02de346c0006b0b7d1f
Rémy-Christophe Schermesser <git@schermesser.com>
2013-07-10 15:25:20 +0200

    Solution of 01_langage/05_les_prototypes

--- src/test/javascript/01_langage/05_les_prototypes_spec.js
+++ src/test/javascript/01_langage/05_les_prototypes_spec.js
@@ -1,8 +1,8 @@
 
 describe("les prototypes", function() {
 	// Javascript est un langage de programmation orientée prototype.
-	// C'est une forme de programmation orientée objet sans classe, basée sur la notion de prototype. 
-	// Un prototype est un objet à partir duquel on crée de nouveaux objets. 
+	// C'est une forme de programmation orientée objet sans classe, basée sur la notion de prototype.
+	// Un prototype est un objet à partir duquel on crée de nouveaux objets.
 	// Toute fonction Javascript comporte une propriété nommée _prototype_ (sans les _)
 
 	// N'hésiter pas à lire la page wikipedia sur le sujet : http://fr.wikipedia.org/wiki/Programmation_orient%C3%A9e_prototype
@@ -10,27 +10,26 @@ describe("les prototypes", function() {
 	it("la base", function() {
 		function Ninja() {}
 
-		//Completer le code ici pour faire passer les tests
+		Ninja.prototype.swingSword = function() { return true; };
 
 		var ninjaA = Ninja();
-		expect(ninjaA.swingSword()).toBe( /**/ );
+		expect(ninjaA).toBe(undefined);
 
-		var ninjaB = new Ninja()
+		var ninjaB = new Ninja();
 		expect(ninjaB.swingSword()).toBeTruthy();
 	});
 
-	// En JS, tout objet a une propriété nommée _constructor_ 
+	// En JS, tout objet a une propriété nommée _constructor_
 	// qui représente la fonction qui a été utilisée pour le créer
-
 	it("les constructeurs", function() {
-		function Ninja(){} 
-		 
-		var ninja = new Ninja(); 
+		function Ninja() {}
 
-		//Faire passer les tests		
-		expect(typeof ninja).toBe( /**/ );
-		expect(ninja instanceof Ninja).toBe( /**/ );
-		expect(ninja.constructor).toBe( /**/ );
+		var ninja = new Ninja();
+
+		//Faire passer les tests
+		expect(typeof ninja).toBe('object');
+		expect(ninja instanceof Ninja).toBeTruthy();
+		expect(ninja.constructor).toBe(Ninja);
 	});
 
 	it("les constructeurs 2", function() {
@@ -39,8 +38,9 @@ describe("les prototypes", function() {
 		var ninjaB = new ninja.constructor();
 
 		//Faire passer les tests
-		expect(typeof ninja).toBe( /**/ );
-		expect(typeof ninjaB).toBe( /**/ );
+		expect(typeof ninja).toBe('object');
+		expect(typeof ninja.constructor).toBe('function');
+		expect(typeof ninjaB).toBe('object');
 	});
 
 	it("héritage", function() {
@@ -53,10 +53,10 @@ describe("les prototypes", function() {
 
 		var ninja = new Ninja();
 
-		//Faire passer les tests		
-		expect(ninja instanceof Ninja).toBe( /**/ );
-		expect(ninja instanceof Person).toBe( /**/ );
-		expect(ninja instanceof Object).toBe( /**/ );
+		//Faire passer les tests
+		expect(ninja instanceof Ninja).toBeTruthy();
+		expect(ninja instanceof Person).toBeTruthy();
+		expect(ninja instanceof Object).toBeTruthy();
 	});
 
 	it("héritage 2", function() {
@@ -64,6 +64,10 @@ describe("les prototypes", function() {
 		Person.prototype.getName = function(){
 		  return this.name;
 		};
+
+		function Me() { this.name = "A name was set."; };
+		Me.prototype = new Person();
+
 
 		// Coder une fonction qui hérite de Person
 		// et déclarer son name dans le constructeur
