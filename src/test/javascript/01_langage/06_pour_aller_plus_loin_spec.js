fe5b625e648922dd52ce7eae0f545a0fc30d229c
Rémy-Christophe Schermesser <git@schermesser.com>
2013-07-11 10:18:06 +0200

    Solution for 01_langage/06_pour_aller_plus_loin

--- src/test/javascript/01_langage/06_pour_aller_plus_loin_spec.js
+++ src/test/javascript/01_langage/06_pour_aller_plus_loin_spec.js
@@ -1,19 +1,26 @@
 
 describe("Pour aller plus loin", function() {
-	
-	it("mettre en cache le résultat d'une fonction", function() {
-		//Hint : utiliser les propriétés
 
-		function isPrime( num ) { 
-			var prime = (num != 1); // Everything but 1 can be prime 
-			for ( var i = 2; i < num; i++ ) { 
-				if ( num % i == 0 ) { 
-					prime = false; 
-					break; 
-				} 
-			} 
-			return prime; 
+	it("mettre en cache le résultat d'une fonction", function() {
+		function isPrime( num ) {
+			if ( isPrime.cache[ num ] != null ) {
+				return isPrime.cache[ num ];
+			}
+
+			var prime = num != 1; // Everything but 1 can be prime
+			for ( var i = 2; i < num; i++ ) {
+				if ( num % i == 0 ) {
+					prime = false;
+					break;
+				}
+			}
+
+			isPrime.cache[ num ] = prime
+
+			return prime;
 		}
+
+		isPrime.cache = {};
 
 		//Modifiez la méthode au dessus pour faire passer les tests
 		expect(isPrime(5)).toBeTruthy();
@@ -22,28 +29,25 @@ describe("Pour aller plus loin", function() {
 
 	it("Parlons contexte", function() {
 		//Hint : encore utilisez la méthode apply : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
-		var object = {}; 
-		function fn(){ 
-			return this; 
+		var object = {};
+		function fn() {
+			return this;
 		}
 
-		//Faites passer les tests
-		expect(fn()).toBe( /**/ );
-
 		//Changez le contexte d'appel de fn par object
-		expect( /**/ ).toBe( object );
+		expect(fn.apply(object)).toBe(object);
 	})
 
 	it("looooooop", function() {
-		//Hint : encore apply
-		function loop(array, fn){ 
-			for ( var i = 0; i < array.length; i++ ) { 
-				// Implementez la méthode pour faire passer les tests
-			} 
-		} 
+		//Hint : encore apply/call
+		function loop(array, fn) {
+			for ( var i = 0; i < array.length; i++ ) {
+				fn.call( array, array[i], i );
+			}
+		}
 
-		var num = 0; 
-		loop([0, 1, 2], function(value){ 
+		var num = 0;
+		loop([0, 1, 2], function(value) {
 			expect(value).toBe(num);
 			expect(this instanceof Array).toBeTruthy();
 
@@ -52,111 +56,122 @@ describe("Pour aller plus loin", function() {
 	});
 
 	it("je change de nom", function() {
-		function Ninja(name){ 
-			// Implement meeee! 
-		} 
-		 
-		var ninja = new Ninja("John"); 
+		function Ninja(name) {
+			this.changeName(name);
+		}
+		Ninja.prototype.changeName = function(name) {
+			this.name = name;
+		}
+
+		var ninja = new Ninja("John");
 		expect(ninja.name).toBe("John");
-		 
-		ninja.changeName("Bob"); 
+
+		ninja.changeName("Bob");
 		expect(ninja.name).toBe("Bob");
 	});
 
 	it("avec ou sans new", function() {
-		function User(first, last){ 
-			//Coder ici
-			 
-			this.name = first + " " + last; 
-		} 
-		 
-		var name = "Resig"; 
-		var user = User("John", name); 
-		 
-		expect(typeof user).toBe(User);
+		function User(first, last) {
+			if ( !(this instanceof User) ) {
+				return new User(first, last);
+			}
+
+			this.name = first + " " + last;
+		}
+
+		var name = "Resig";
+		var user = User("John", name);
+
+		expect(user instanceof User).toBeTruthy();
 		expect(name).toBe("Resig");
 	});
 
 	it("min ou max sur un tableau", function() {
-		function smallest(array) { 
-			//Code ici, ça doit faire une seule ligne et utiliser Math.min
-		} 
+		function smallest(array) {
+			return Math.min.apply(Math, array);
+		}
 
 		expect(Math.min(0, 1, 2, 3)).toBe(0);
-		expect(Math.min([0, 1, 2, 3])).toBe(NaN);
+		expect(Math.min([0, 1, 2, 3])).toBeNaN();
 		expect(smallest([0, 1, 2, 3])).toBe(0);
 	});
 
 	it("sur les closures", function() {
-		var a = 5; 
+		var a = 5;
 		function runMe(a) {
-			expect(a).toBe( /**/ ); 
-		 
-			function innerRun(){ 
-				expect(b).toBe( /**/ );
-				expect(c).toBe( /**/ );
-			} 
+			expect(a).toBe(6);
 
-			var b = 7; 
-			innerRun(); 
-			var c = 8; 
-		} 
-		runMe(6); 
-		 
-		for ( var d = 0; d < 3; d++ ) { 
-			setTimeout(function(){ 
-				expect(d).toBe( /**/ );
-			}, 100); 
+			function innerRun(){
+				expect(b).toBe(7);
+				expect(c).toBe(undefined);
+			}
+
+			var b = 7;
+			innerRun();
+			var c = 8;
 		}
+		runMe(6);
 	});
 
 	it("un effet de bord", function() {
-		var Button = { 
-			click: function(){ 
-				this.clicked = true; 
-			} 
-		}; 
-		 
-		var elem = document.createElement("li"); 
-		elem.innerHTML = "Click me!"; 
-		elem.onclick = Button.click; 
-		document.getElementById("results").appendChild(elem); 
-		 
-		elem.onclick(); 
-		expect(elem.clicked).toBe( /**/ );
+		setFixtures('<div id="results"></div>');
+
+		var Button = {
+			click: function(){
+				this.clicked = true;
+			}
+		};
+
+		var elem = document.createElement("li");
+		elem.innerHTML = "Click me!";
+		elem.onclick = Button.click;
+		document.getElementById("results").appendChild(elem);
+
+		elem.onclick();
+		expect(elem.clicked).toBeTruthy();
 	});
 
 	it("coder un bind", function() {
+		setFixtures('<div id="results"></div>');
+
 		//Hint : il faut utiliser apply
 		//Hint 2 : une fonction d'un objet est une propriété
-		function bind(context, name) { 
-			//Coder ici
-		} 
-		 
-		var Button = { 
-		  click: function(){ 
-		    this.clicked = true; 
-		  } 
-		}; 
-		 
-		var elem = document.createElement("li"); 
-		elem.innerHTML = "Click me!"; 
-		elem.onclick = bind(Button, "click"); 
-		document.getElementById("results").appendChild(elem); 
-		 
-		elem.onclick(); 
+		function bind(context, name) {
+			return function(){
+				return context[name].apply(context, arguments);
+			};
+		}
+
+		var Button = {
+			click: function(){
+				this.clicked = true;
+			}
+		};
+
+		var elem = document.createElement("li");
+		elem.innerHTML = "Click me!";
+		elem.onclick = bind(Button, "click");
+		document.getElementById("results").appendChild(elem);
+
+		elem.onclick();
 		expect(Button.clicked).toBeTruthy();
 		expect(elem.clicked).toBe(undefined);
 	});
 
 	it("un plus joli bind", function() {
-		//Hint : Enrichir le propotype de Function
-		//Hint 2 : Encore du apply
+		setFixtures('<div id="results"></div>');
+
+		Function.prototype.bind = function(object){
+			var fn = this;
+			return function(){
+				return fn.apply(object, arguments);
+			};
+		};
 
 		var Button = {
-		  click: function(){
-		    this.clicked = true;
-		  }
+			click: function(){
+				this.clicked = true;
+			}
 		};
 
 		var elem = document.createElement("li");
